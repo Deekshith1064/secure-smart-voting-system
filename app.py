06fd40fa8c9ffc7f39667d32af36b7deb3eb37d2
Deekshith1064 <k.deekshith.9390@gmail.com>
2026-03-22 20:47:37 +0530

    fixed sqlite database initialization for render

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template, request, redirect, session
 import sqlite3
 from config import Config
 from flask import jsonify
+import os
+
+if not os.path.exists(Config.DATABASE):
+    open(Config.DATABASE, "w").close()
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
 def get_db_connection():
-    conn = sqlite3.connect(app.config["DATABASE"])
+    conn = sqlite3.connect(Config.DATABASE)
     conn.row_factory = sqlite3.Row
     return conn
 
